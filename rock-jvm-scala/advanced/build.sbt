a8fd706035f866a9f723b33187e1ef839a1764b7
Shankar Nakai <shankar@otus.com>
2021-01-12 17:14:49 -0600

    master unfinish code

---
+++ rock-jvm-scala/advanced/build.sbt
@@ -0,0 +1,5 @@
+name := "advanced"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ rock-jvm-scala/advanced/src/main/scala/Lazy.scala
@@ -0,0 +1,25 @@
+object Lazy {
+
+
+}
+
+abstract class MyStream[+A] {
+  def isEmpty: Boolean
+  def head: A
+  def tail: MyStream[A]
+
+  def #::[B >: A](element: B): MyStream[B]
+  def ++[B >: A](anotherStream: MyStream[B]): MyStream[B]
+
+  def foreach(f: A => Unit): Unit
+  def map[B](f: A => B): MyStream[B]
+  def flatMap[B](f: A => MyStream[B]): MyStream[B]
+  def filter(predicate: A => Boolean): MyStream[A]
+
+  def take(n: Int): MyStream[A]
+  def takeAsList(n: Int): List[A]
+}
+
+object MyStream {
+ def from[A](start: A)(generator: A => A): MyStream[A] = ???
+}
